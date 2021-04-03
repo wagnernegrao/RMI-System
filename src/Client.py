1b7eb55fcf2b49da766d65b65a5ee53942e0d609
wagnernegrao <wagnerfelidre@gmail.com>
2021-04-03 10:26:58 -0300

    Modelo de cliente

--- src/Client.py
+++ src/Client.py
@@ -5,11 +5,47 @@ import Pyro4
 server = Pyro4.Proxy("PYRONAME:server") # Conecta com o servidor
 
 
+# email = input("Email: ")
+# nome = input("Nome: ")
+# sobrenome = input("Sobrenome: ")
+# residencia = input("Residencia: ")
+# formacao_academica = input("Formacao academica: ")
+# habilidades = input("Habilidades: ")
+# experiencia_profissional = input("Experiencia profissional: ")
+
+
+# user = {"nome": nome,
+#         "sobrenome": sobrenome,
+#         "email": email,
+#         "residencia": residencia,
+#         "formacao_academica": formacao_academica,
+#         "habilidades": habilidades,
+#         "experiencia_profissional": experiencia_profissional 
+#         }
+
+
 # usuario teste
-user = {"name": "wagner",
-        "email": "wagnerfelidre@gmail.com"
+user = {"nome": "wagner",
+        "sobrenome": "negrao",
+        "email": "wagner@email.com",
+        "residencia": "rua jarbas passarinho",
+        "formacao_academica": "cientista computacao",
+        "habilidades": "Javaboy",
+        "experiencia_profissional": "dev"
         }
 
-print(server.createUser(user))
+user2 = {"nome": "wagner",
+        "sobrenome": "negrao",
+        "email": "wagner@email.com",
+        "residencia": "rua jarbas passarinho",
+        "formacao_academica": "pedreiro",
+        "habilidades": "Javaboy",
+        "experiencia_profissional": "dev"
+}
 
-print(server.listOfUser())
+print(server.createUser(user))
+print(server.createUser(user))
+print(server.createUser(user2))
+
+graduacao = input("Qual graduacao: ")
+print(server.findByGraduation(graduacao))
