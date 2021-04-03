f0f59f580794d9a8bea318b64d89be3c63e0241f
Alberto Sobrinho <aclsobrinho@hotmail.com>
2021-04-03 15:16:41 -0300

    add docker environment

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM python:3.8.9-buster
+
+RUN pip install --upgrade pip
+RUN pip install Pyro4
--- src/Server.py
+++ src/Server.py
@@ -15,13 +15,11 @@ class Server(object):
 
 
 
-
-
-
-daemon = Pyro4.Daemon() # inicia o processo
-ns = Pyro4.locateNS() # Inicia o processo local
+daemon = Pyro4.Daemon(host="server", port=9100) # inicia o processo
+ns = Pyro4.locateNS(host="pyro-ns", port=9090) # Inicia o processo local
 uri = daemon.register(Server) # Registra o processo
 ns.register("server", uri) # Cria um alias para o processo registrado
 
 # print("Ready. Object uri =", uri)
+print("Abrindo processo para chamadas")
 daemon.requestLoop() # Deixa o processo aberto para chamadas
